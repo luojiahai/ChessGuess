88178805c165db3e75b8f00f3d17f009b917e604
luojiahai <ffreylaw@gmail.com>
2018-04-01 17:49:18 +1000

    update

--- Project2.hs
+++ Project2.hs
@@ -1,6 +1,6 @@
 module Project2 (initialGuess, nextGuess, GameState) where
 
-data Color = Black | White 
+data Color = Black | White
              deriving (Eq)
 data Kind = King | Queen | Rook | Knight | Bishop | Pawn
              deriving (Eq)
@@ -22,71 +22,97 @@ instance Show Kind where
 instance Show Piece where
     show (Piece c k) = show c ++ show k
 
-type GameState = (Int, Kind, (Int,Int,Int), [String])
-
+type GameState = (Int, Piece, (Int,Int,Int), [String])
 
 
 initialGuess :: Int -> ([String],GameState)
-initialGuess 0 = ([],(0, Pawn, (0,0,0),[]))
-initialGuess size = let guess = if size <= 8 then generatePieces size (Piece Black Pawn) 
-                                else generatePieces 8 (Piece Black Pawn) 
-                    in
-                    (guess, (size, Pawn, (0,0,0), []))
+initialGuess 0 = ([],(0, Piece Black Pawn, (0,0,0),[]))
+initialGuess size = let guess = if size <= 8 then generatePieces size (Piece Black Pawn) else generatePieces 8 (Piece Black Pawn) 
+                    in (guess, (size, Piece Black Pawn, (0,0,0), []))
 
 generatePieces :: Int -> Piece -> [String]
 generatePieces 0 _ = []
 generatePieces n piece = show piece : generatePieces (n-1) piece
 
-pawnGuess :: ([String],GameState) -> (Int, Int, Int) -> [String]
-pawnGuess (prev_guess, (size, kind, prev_ans, soln)) (x,y,z)
-            | (size - x - y) < 2 = pawnSoln (x,y,z) ++ generatePieces 1 (Piece Black Knight) 
-            | (size - x - y) == 2 = pawnSoln (x,y,z) ++ generatePieces 2 (Piece Black Knight)
-            | (size - x - y) == 3 = pawnSoln (x,y,z) ++ generatePieces 2 (Piece Black Knight) ++ generatePieces 1 (Piece White Knight) 
-            | (size - x - y) == 4 = pawnSoln (x,y,z) ++ generatePieces 2 (Piece Black Knight) ++ generatePieces 2 (Piece White Knight) 
+nextPawnSoln :: (Int, Int, Int) -> [String]
+nextPawnSoln (0,0,_) = []
+nextPawnSoln (x,y,z)
+    | x > 0 = show (Piece Black Pawn) : nextPawnSoln (x-1,y,z)
+    | y > 0 = show (Piece White Pawn) : nextPawnSoln (x,y-1,z)
 
+nextPawnGuess :: ([String],GameState) -> (Int, Int, Int) -> [String]
+nextPawnGuess (prev_guess, (size, piece, prev_ans, soln)) (x,y,z) 
+    | piece == Piece Black Pawn = nextPawnSoln (x,y,z)
+    | piece == Piece White Pawn = 
+        if size - length soln < 2 
+        then soln ++ generatePieces 1 (Piece Black Knight) 
+        else soln ++ generatePieces 2 (Piece Black Knight)
 
-pawnSoln :: (Int, Int, Int) -> [String]
-pawnSoln (0,0,_) = []
-pawnSoln (x,y,z)
-            | x > 0 = show (Piece Black Pawn) : pawnSoln (x-1,y,z)
-            | y > 0 = show (Piece White Pawn) : pawnSoln (x,y-1,z)
+nextNBRSoln :: [String] -> Piece -> (Int, Int, Int) -> (Int, Int, Int) -> [String]
+nextNBRSoln prev_soln piece (prev_x,prev_y,prev_z) (x,y,z)
+    | x - prev_x == 0 = prev_soln
+    | x - prev_x == 1 = prev_soln ++ generatePieces 1 piece
+    | x - prev_x >= 2 = prev_soln ++ generatePieces 2 piece
+
+nextNBRGuess :: ([String],GameState) -> (Int, Int, Int) -> Piece -> [String]
+nextNBRGuess (prev_guess, (size, piece, (prev_x,prev_y,prev_z), soln)) (x,y,z) next_piece = 
+    let next_soln = nextNBRSoln soln piece (prev_x,prev_y,prev_z) (x,y,z) in 
+        if size - length next_soln == 0 
+            then next_soln
+        else if size - length next_soln == 1 
+            then next_soln ++ generatePieces 1 next_piece
+        else if next_piece == Piece Black Queen 
+            then next_soln ++ generatePieces 1 next_piece
+        else 
+            next_soln ++ generatePieces 2 next_piece
             
-hasPiece :: Piece -> [String] -> Bool
-hasPiece _ [] = False
-hasPiece piece (g:guess) = if show piece == g 
-                                then True 
-                           else 
-                                hasPiece piece guess
+newQKSoln :: [String] -> Piece -> (Int, Int, Int) -> (Int, Int, Int) -> [String]
+newQKSoln prev_soln piece (prev_x,prev_y,prev_z) (x,y,z)
+    | x - prev_x == 0 = prev_soln
+    | x - prev_x == 1 = prev_soln ++ generatePieces 1 piece
 
-knightGuess :: ([String],GameState) -> (Int, Int, Int) -> [String]
-knightGuess _ (0,0,0) = []
-knightGuess (prev_guess, (size, kind, (prev_x,prev_y,prev_z), soln)) (x,y,z)
-            | x - (prev_x + prev_y) == 1 && y == 1 = soln ++ generatePieces 1 (Piece Black Knight) ++ generatePieces 1 (Piece White Knight)
-            | x - (prev_x + prev_y) == 1 = if hasPiece (Piece Black Knight) prev_guess 
-                                                then soln ++ generatePieces 1 (Piece Black Knight) 
-                                           else 
-                                                soln ++ generatePieces 1 (Piece White Knight)
-            | x - (prev_x + prev_y) == 2 && y == 1 = soln ++ generatePieces 1 (Piece Black Knight) ++ generatePieces 2 (Piece White Knight)
-            | x - (prev_x + prev_y) == 2 && y == 0 = if hasPiece (Piece Black Knight) prev_guess 
-                                                         then soln ++ generatePieces 2 (Piece Black Knight) 
-                                                     else 
-                                                         soln ++ generatePieces 2 (Piece White Knight)
-            | x - (prev_x + prev_y) == 0 && y == 1 = soln ++ generatePieces 1 (Piece White Knight)
-            | x - (prev_x + prev_y) == 0 && y == 2 = soln ++ generatePieces 2 (Piece White Knight)
-            | x - (prev_x + prev_y) == 0 && y == 0 = if (size - x - y < 2)
-                                                         then soln ++ generatePieces 1 (Piece Black Bishop)
-                                                     else
-                                                         soln ++ generatePieces 2 (Piece Black Bishop)
-
-bishopGuess :: ([String],GameState) -> (Int, Int, Int) -> [String]
-bishopGuess _ (0,0,0) = []
-bishopGuess (prev_guess, (size, kind, (prev_x,prev_y,prev_z), soln)) (x,y,z) = []
+nextQKGuess :: ([String],GameState) -> (Int, Int, Int) -> Piece -> [String]
+nextQKGuess (prev_guess, (size, piece, (prev_x,prev_y,prev_z), soln)) (x,y,z) next_piece =   
+    let next_soln = newQKSoln soln piece (prev_x,prev_y,prev_z) (x,y,z) in 
+        if x - prev_x == 0 && y - prev_y == 0 && z - prev_z == 0 then next_soln
+        else if size - length next_soln == 0 then next_soln
+        else next_soln ++ generatePieces 1 next_piece
 
 nextGuess :: ([String],GameState) -> (Int,Int,Int) -> ([String],GameState)
-nextGuess (prev_guess, (size, kind, prev_ans, soln)) new_ans
-            | kind == Pawn   = let new_guess = pawnGuess (prev_guess,(size, kind, prev_ans, soln)) new_ans
-                               in (new_guess, (size, Knight, new_ans, pawnSoln new_ans))
-            | kind == Knight = let new_guess = knightGuess (prev_guess,(size, kind, prev_ans, soln)) new_ans
-                               in (new_guess, (size, Bishop, new_ans, soln))
-            | kind == Bishop = let new_guess = bishopGuess (prev_guess,(size, kind, prev_ans, soln)) new_ans
-                               in (new_guess, (size, Rook, new_ans, soln))
+nextGuess (prev_guess, (size, piece, prev_ans, soln)) new_ans
+    | piece == Piece Black Pawn
+        = let new_guess = nextPawnGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans
+            in (new_guess, (size, Piece White Pawn, new_ans, nextPawnSoln new_ans))
+    | piece == Piece White Pawn
+        = let new_guess = nextPawnGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans
+            in (new_guess, (size, Piece Black Knight, new_ans, soln))
+    | piece == Piece Black Knight
+        = let new_guess = nextNBRGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans (Piece White Knight)
+            in (new_guess, (size, Piece White Knight, new_ans, nextNBRSoln soln piece prev_ans new_ans))
+    | piece == Piece White Knight
+        = let new_guess = nextNBRGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans (Piece Black Bishop)
+            in (new_guess, (size, Piece Black Bishop, new_ans, nextNBRSoln soln piece prev_ans new_ans))
+    | piece == Piece Black Bishop
+        = let new_guess = nextNBRGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans (Piece White Bishop)
+            in (new_guess, (size, Piece White Bishop, new_ans, nextNBRSoln soln piece prev_ans new_ans))
+    | piece == Piece White Bishop
+        = let new_guess = nextNBRGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans (Piece Black Rook)
+            in (new_guess, (size, Piece Black Rook, new_ans, nextNBRSoln soln piece prev_ans new_ans))                
+    | piece == Piece Black Rook
+        = let new_guess = nextNBRGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans (Piece White Rook)
+            in (new_guess, (size, Piece White Rook, new_ans, nextNBRSoln soln piece prev_ans new_ans))
+    | piece == Piece White Rook
+        = let new_guess = nextNBRGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans (Piece Black Queen)
+            in (new_guess, (size, Piece Black Queen, new_ans, nextNBRSoln soln piece prev_ans new_ans))
+    | piece == Piece Black Queen
+        = let new_guess = nextQKGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans (Piece White Queen)
+            in (new_guess, (size, Piece White Queen, new_ans, newQKSoln soln piece prev_ans new_ans))
+    | piece == Piece White Queen
+        = let new_guess = nextQKGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans (Piece Black King)
+            in (new_guess, (size, Piece Black King, new_ans, newQKSoln soln piece prev_ans new_ans))
+    | piece == Piece Black King
+        = let new_guess = nextQKGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans (Piece White King)
+            in (new_guess, (size, Piece White King, new_ans, newQKSoln soln piece prev_ans new_ans))
+    | piece == Piece White King
+        = let new_guess = nextQKGuess (prev_guess,(size, piece, prev_ans, soln)) new_ans (Piece White King)
+            in (new_guess, (size, Piece White King, new_ans, newQKSoln soln piece prev_ans new_ans))
